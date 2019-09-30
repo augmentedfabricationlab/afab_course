24e8ad32de8c831d56ff74b833dba582ada8013e
stefanap <parascho@arch.ethz.ch>
2019-09-29 20:28:49 -0400

    small changes and robot fabrication basics

--- python_basics/08_RhinoGeometry.py
+++ python_basics/08_RhinoGeometry.py
@@ -7,20 +7,24 @@ import Rhino.Input.RhinoGet as get
 
 ###################### rhinoscriptsyntax ################
 #ptA = rs.AddPoint(0, 0, 0)
-#ptA = rs.AddPoint(0, 0, 0)
 #ptB = rs.AddPoint(10, 10, 10)
+#
+#ptC = rs.AddPoint(5,5,5)
+#print(ptC)
 #
 #lineA = rs.AddLine(ptA, ptB)
 #circle = rs.AddCircle(ptA, 8)
+##circle = rs.AddCircle(ptA, 8)
 #
-#grid of points
+##grid of points
 #for i in range(10):
 #    for j in range(10):
 #        x_coord = i
 #        y_coord = j
 #        rs.AddPoint(x_coord, y_coord, 0)
-#        rs.Sleep(500)
+##        rs.Sleep(500)
 #
+#make 3-dimensional
 ##find rule to connect points diagonally
 #
 ##deform grid of points
@@ -28,25 +32,27 @@ import Rhino.Input.RhinoGet as get
 
 ########### import objects from Rhino ####################
 #ptC = rs.GetObject("pick point", 0)
-#print type(ptC)
+##rs.GetObject()
+##print type(ptC)
 #print rs.PointCoordinates(ptC)
 
 
 ####################### rhinocommon ####################### 
 
-#ptA = rg.Point3d(0, 0, 0)
-#ptB = rg.Point3d(10, 10, 10)
+ptA = rg.Point3d(0, 0, 0)
+ptB = rg.Point3d(10, 10, 10)
+
+#print(type(ptA))
 #
-#lineA = rg.Line(ptA, ptB)
-#circle = rg.Circle(ptA, 8)
+lineA = rg.Line(ptA, ptB)
+circle = rg.Circle(ptA, 8)
 #
-#scriptcontext.doc.Objects.AddLine(lineA)
-#scriptcontext.doc.Objects.AddCircle(circle)
+scriptcontext.doc.Objects.AddLine(lineA)
+scriptcontext.doc.Objects.AddCircle(circle)
 
 
-#ptC = get.GetPoint("pick point", True)
-#print ptC
-crvC = get.getC
+ptC = get.GetPoint("pick point", True)
+print ptC
 
 ####### extra exercise: find rule to connect points diagonally #########
 
