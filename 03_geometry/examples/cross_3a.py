f6c7be200358d1ece25a0812998f654278086393
Kathrin Dörfler <doerfler.kathrin@gmail.com>
2020-05-02 02:59:56 +0200

    assignment 03 geometry

--- 03_geometry/examples/cross_3a.py
+++ 03_geometry/examples/cross_3a.py
@@ -3,6 +3,5 @@ from compas.geometry import cross_vectors
 u = [1.0, 0.0, 0.0]
 v = [0.0, 1.0, 0.0]
 
-uxv = cross_vectors(u, v)
-
-print(uxv)
+print(cross_vectors(u, v)[2] > 0)
+print(cross_vectors(v, u)[2] > 0)
